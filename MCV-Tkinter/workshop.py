8e9122b7632696ec72ad793fdeaae561e92b417c
Jarosław Olszewski <oljar@tlen.pl>
2023-07-01 11:38:38 +0200

    change columns

--- MCV-Tkinter/workshop.py
+++ MCV-Tkinter/workshop.py
@@ -1,6 +1,43 @@
-self.down_scope_label = ttk.Label(tab0, text='górny zakres:')
-self.down_scope_label.grid(row=3, column=0)
+import pandas as pd
+import csv
 
-self.down_scope_var = tk.StringVar()
-self.down_scope_entry = ttk.Entry(tab0, textvariable=self.down_scope_var, width=30)
-self.down_scope_entry.grid(row=3, column=1, sticky=tk.NSEW)
+x_tag = 'M51: Pa'
+y_tag = 'M53: Pa'
+name = 'data_3.csv'
+
+value = 'M51: Pa'
+
+
+data_list = []
+with open(name,'r')as f:
+    data = csv.reader(f)
+    for i in data:
+        data_list.append(i)
+
+result_row_number = []
+for row_number , row  in enumerate(data_list):
+    if str(x_tag) in str(row) :
+        result_row_number.append(row_number)
+
+
+print (int(result_row_number[0]))
+df1 = pd.read_csv(name,sep=';', decimal=',' , header =int(result_row_number[0]) )
+print (df1)
+
+
+
+
+
+# result = print(df1.eq(value).any(axis=1))
+
+
+
+#df1 = pd.read_csv(name,sep=';', decimal=',' , header= 0)
+
+
+#
+# # Przykład użycia
+# file_path = 'plik.csv'
+# value = 'szukana wartość'
+# row_number = find_row_number(file_path, value)
+# print(f'Numer wiersza: {row_number}')
