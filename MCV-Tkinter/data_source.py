55cbe2e7b693a2a39edcfbf1b3faa67cb2a811e6
Jarosław Olszewski <oljar@tlen.pl>
2023-09-13 06:29:48 +0200

    dict -workshop

--- MCV-Tkinter/data_source.py
+++ MCV-Tkinter/data_source.py
@@ -1,27 +1,45 @@
-class GetData:
-    name_col_x_tab0 = 'x'
-    name_col_y_tab_0 = 'y'
-    name_serial_var = 'seria X'
-    polynom_degree = 2
-    step_value = 1
-    scope_down_entry_tab0 = 0
-    scope_up_entry_tab0 = 100
-    formula_x = '41.1*math.sqrt(x)'
-    formula_y = 'y'
-    time_tag = 'ZEIT:'
-    column_x_tag_tab1 = 'M51: Pa'
-    column_y_tag_tab1 = 'M53: Pa'
-    down_scope_tab1 = 0
-    up_scope_tab_1 = 100
-    scale_time_chart = 100
-    name_of_chart = 'Wykres przepływowy'
-    name_of_X_axis_tab3 = 'objętościowy strumień powietrza'
-    unit_of_X_axis_tab3 = 'm3/h'
-    name_of_Y_axis_tab3 = 'spręż dyspozycyjny'
-    unit_of_Y_axis_tab3 = 'Pa'
+import self as self
 
-    scope_down_x_background_entry_tab0 = 0
-    scope_up_x_background_entry_tab0 = 100
-    scope_down_y_background_entry_tab0 = 5
-    scope_up_y_background_entry_tab0 = 105
-    trasparency_picture = 0.5
+
+class GetData:
+        # def __init__(self,aaa):
+        #     self.slownik = aaa
+        # def open_data_sorce(self):
+        #     with open('nota.txt', 'r') as plik:
+        #         linie = plik.readlines()
+        #
+        #         for linia in linie:
+        #             klucz, wartosc = linia.strip().split('=')
+        #             self.slownik[klucz] = wartosc
+        #
+        # name_col_x_tab0 = self.slownik['x']
+
+        name_col_x_tab0 ='x'
+
+        name_col_y_tab_0 = 'y'
+        name_serial_var = 'seria X'
+        polynom_degree = 2
+        step_value = 1
+        scope_down_entry_tab0 = 0
+        scope_up_entry_tab0 = 100
+        formula_x = '41.1*math.sqrt(x)'
+        formula_y = 'y'
+        time_tag = 'ZEIT:'
+        column_x_tag_tab1 = 'M51: Pa'
+        column_y_tag_tab1 = 'M53: Pa'
+        down_scope_tab1 = 0
+        up_scope_tab_1 = 100
+        scale_time_chart = 100
+        name_of_chart = 'Wykres przepływowy'
+        name_of_X_axis_tab3 = 'objętościowy strumień powietrza'
+        unit_of_X_axis_tab3 = 'm3/h'
+        name_of_Y_axis_tab3 = 'spręż dyspozycyjny'
+        unit_of_Y_axis_tab3 = 'Pa'
+
+        scope_down_x_background_entry_tab0 = 0
+        scope_up_x_background_entry_tab0 = 100
+        scope_down_y_background_entry_tab0 = 5
+        scope_up_y_background_entry_tab0 = 105
+        trasparency_picture = 0.5
+
+
--- MCV-Tkinter/workshop.py
+++ MCV-Tkinter/workshop.py
@@ -1,44 +1,69 @@
-import pandas as pd
-import csv
-
-x_tag = 'M51: Pa'
-y_tag = 'M53: Pa'
-name = 'data_3.csv'
-
-
-
-
-data_list = []
-with open(name,'r')as f:
-    data = csv.reader(f)
-    for i in data:
-        data_list.append(i)
-
-result_row_number = []
-for row_number , row  in enumerate(data_list):
-    if str(x_tag) in str(row) :
-        result_row_number.append(row_number)
-
-
-print (int(result_row_number[0]))
-df1 = pd.read_csv(name,sep=';', decimal=',' , header =int(result_row_number[0]) )
-print (df1)
-
-
-
-
-
-
-# result = print(df1.eq(value).any(axis=1))
-
-
-
-#df1 = pd.read_csv(name,sep=';', decimal=',' , header= 0)
-
-
+# import tkinter as tk
 #
-# # Przykład użycia
-# file_path = 'plik.csv'
-# value = 'szukana wartość'
-# row_number = find_row_number(file_path, value)
-# print(f'Numer wiersza: {row_number}')
+# def validate(input):
+#     if input.isdigit():
+#         return True
+#     elif input == "":
+#         return True
+#     else:
+#         return False
+#
+# root = tk.Tk()
+# entry = tk.Entry(root, validate="key")
+# entry['validatecommand'] = (root.register(validate), '%P')
+# entry.pack()
+# root.mainloop()
+
+# Otwórz plik
+with open('nota.txt', 'r') as plik:
+    # Odczytaj linie pliku
+    linie = plik.readlines()
+
+# Stwórz pusty słownik
+slownik = {}
+
+
+for linia in linie:
+
+    klucz, wartosc = linia.strip().split('=')
+
+    slownik[klucz] = wartosc
+    print(slownik[klucz])
+
+
+
+
+
+name_col_x_tab0 = 'x'
+
+
+
+name_serial_var = 'seria X'
+polynom_degree = 2
+step_value = 1
+scope_down_entry_tab0 = 0
+scope_up_entry_tab0 = 100
+formula_x = '41.1*math.sqrt(x)'
+formula_y = 'y'
+time_tag = 'ZEIT:'
+column_x_tag_tab1 = 'M51: Pa'
+column_y_tag_tab1 = 'M53: Pa'
+down_scope_tab1 = 0
+up_scope_tab_1 = 100
+scale_time_chart = 100
+name_of_chart = 'Wykres przepływowy'
+name_of_X_axis_tab3 = 'objętościowy strumień powietrza'
+unit_of_X_axis_tab3 = 'm3/h'
+name_of_Y_axis_tab3 = 'spręż dyspozycyjny'
+unit_of_Y_axis_tab3 = 'Pa'
+
+scope_down_x_background_entry_tab0 = 0
+scope_up_x_background_entry_tab0 = 100
+scope_down_y_background_entry_tab0 = 5
+scope_up_y_background_entry_tab0 = 105
+trasparency_picture = 0.5
+
+# Wydrukuj słownik
+
+
+
